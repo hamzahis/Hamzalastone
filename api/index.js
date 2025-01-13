4941777d40d5ed5a318d331ab2065f5540e9bf0c
hamzahis <164903490+hamzahis@users.noreply.github.com>
2025-01-13 12:34:43 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,42 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route for the form (GET request)
 app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Enter Your Details</title>
-    </head>
-    <body>
-      <h1>Enter Your Details</h1>
-      <form action="/submit" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        <button type="submit">Submit</button>
-      </form>
-    </body>
-    </html>
-  `);
-});
-
-// Route to handle form submissions (POST request)
-app.post('/submit', (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-
-  // Append user data to file.txt
-  const data = `Name: ${name}, Email: ${email}\n`;
-  fs.appendFile('file.txt', data, (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      return res.status(500).send('An error occurred while saving your data.');
-    }
-
-    res.send(`<h1>Thank you, ${name}! Your details have been saved.</h1>`);
+  fs.readFile('file.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
   });
+ // res.send();
 });
+
+
 
 // Start the server on port 3000
 app.listen(3000, () => {
