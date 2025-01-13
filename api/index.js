6ba2645ad7b4a5f337a812215975a669e25acd16
hamzahis <164903490+hamzahis@users.noreply.github.com>
2025-01-13 13:34:57 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.appendFile('index.html', '<p> hiiiii</p>', function (err) {
+  fs.appendFile('index.html', '<html>
+<body>
+<h1>My Header</h1>
+<p>My paragraph.</p>
+</body>
+</html>', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
