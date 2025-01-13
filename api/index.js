aa2b634b2a4ab538ac1654eb5a890f8d46ed1200
hamzahis <164903490+hamzahis@users.noreply.github.com>
2025-01-13 12:43:48 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,11 +11,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route for the form (GET request)
 app.get('/', (req, res) => {
-  fs.readFile('file.html', function(err, data) {
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+  res.write("god");
+});
+/*  fs.readFile('file.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
-  });
+  });*/
  // res.send();
 });
 
