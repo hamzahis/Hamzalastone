df8e30ded96c988d806de4509f190f818b1f982c
hamzahis <164903490+hamzahis@users.noreply.github.com>
2025-01-13 13:43:25 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,31 @@
 var http = require('http');
 var fs = require('fs');
+
+// Create an HTTP server
 http.createServer(function (req, res) {
-  fs.appendFile('index.html', '<html>
-<body>
-<h1> Header</h1>
-<p>paragraph.</p>
-</body>
-</html>', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
+  // HTML content to append to the file
+  const htmlContent = `
+<html>
+  <body>
+    <h1>Header</h1>
+    <p>Paragraph.</p>
+  </body>
+</html>
+`;
+
+  // Append the HTML content to 'index.html'
+  fs.appendFile('index.html', htmlContent, function (err) {
+    if (err) {
+      console.error('Error saving file:', err);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error saving file.');
+      return;
+    }
+
+    console.log('Saved!');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('HTML content saved to index.html');
+  });
+}).listen(8080, () => {
+  console.log('Server is running at http://localhost:8080');
 });
-}).listen(8080);
