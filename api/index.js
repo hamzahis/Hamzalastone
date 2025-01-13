89627e5947ee0e7e24630b93ee138121572128a9
hamzahis <164903490+hamzahis@users.noreply.github.com>
2025-01-13 15:06:50 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,9 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Handle root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/file.html'));
-});
-
-module.exports = app;
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
